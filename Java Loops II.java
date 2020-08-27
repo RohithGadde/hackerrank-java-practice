c764d812d8142176a268b3ad938b7de0319830ff
Rohith Gadde <50209060+RohithGadde@users.noreply.github.com>
2020-08-27 17:28:09 +0530

    Update Java Loops II.java

--- Java Loops II.java
+++ Java Loops II.java
@@ -1,37 +1,31 @@
 import java.util.*;
 import java.io.*;
 
-
-
 class Solution{
-    public static void main(String []argh)
-    {
+    public static void main(String []argh){
+        Scanner in = new Scanner(System.in);
+        int t=in.nextInt();
+        for(int i=0;i<t;i++){
+            int a = in.nextInt();
+            int b = in.nextInt();
+            int n = in.nextInt();
+            for(int j=0;j<n;j++){
+                a = a+b;
 
+                if(j>0)
+                {
+                    System.out.print(" ");
+                }
+                System.out.print(a);
+                b = b*2;
 
-
-        Scanner sc = new Scanner(System.in);
-        int t=sc.nextInt();
-
-        for(int i=0;i<t;i++)
-        {
-
-            try
-            {
-                long x=sc.nextLong();
-                System.out.println(x+" can be fitted in:");
-                if(x>=-128 && x<=127)System.out.println("* byte");
-                if(x>=Short.MIN_VALUE && x<=Short.MAX_VALUE)System.out.println("* short");
-                if(x>=Integer.MIN_VALUE && x<=Integer.MAX_VALUE)System.out.println("* int");
-                if(x>=Long.MIN_VALUE && x<=Long.MAX_VALUE)System.out.println("* long");
             }
-            catch(Exception e)
-            {
-                System.out.println(sc.next()+" can't be fitted anywhere.");
-            }
-
+            System.out.print("\n");
         }
+        in.close();
     }
 }
+
 
 
 
